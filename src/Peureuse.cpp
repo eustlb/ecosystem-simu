08329a6e944e73af896d858dbd8e891029ca0746
Eustache Le Bihan <eustache.le-bihan@imt-atlantique.net>
2023-03-20 15:21:17 +0100

    comportement bestiole peureuse implémenté

--- src/Peureuse.cpp
+++ src/Peureuse.cpp
@@ -1,5 +1,6 @@
 #include "Peureuse.h"
 #include "Bestiole.h"
+#include "constantes.h"
 
 Peureuse::Peureuse()
 {
@@ -14,6 +15,23 @@ Peureuse::Peureuse()
 
 void Peureuse::bouge(int xLim, int yLim, Bestiole* bestiole)
 {
+    // on récupère les voisins perçus par la bestiole 
+    std::vector<Bestiole*> v = bestiole->getNeighbors();
+    int taille = v.size();
+    if ((taille >= S_PEUREUSE) && (taille != 0))
+    {
+        // determiner la direction moyenne des voisins (comme pour grégaire)
+        double somme = 0.0;
+        for (int i = 0; i < taille; i++) 
+        {
+        somme += v[i]->getOrientation();
+        }
+        double moyenne = somme / taille;
+        // cout << moyenne << endl;
+        // on change l'orientation de notre bestiole telle quelle soit opposée à cette direction moyenne 
+        bestiole-> setOrientation(M_PI + moyenne);
+
+    }
     // on appelle la méthode bouge classique de notre bestiole avec ces paramètre actualisés
     bestiole->bouge(xLim, yLim);
 }
--- src/constantes.h
+++ src/constantes.h
@@ -1,8 +1,8 @@
 #include <cmath> // pour la constante pi (M_PI)
 
 // constantes de la simulation 
-const float TX_GREGAIRE = .2;
-const float TX_PEUREUSE = .8;
+const float TX_GREGAIRE = .8;
+const float TX_PEUREUSE = .2;
 const float TX_KAMIKAZE = 0.;
 const float TX_PREVOYANTE = 0.;
 const float TX_PERSOMULT = 0.;
@@ -17,10 +17,13 @@ const double CAPA_VUE_MIN = 0.1;
 const double CAPA_VUE_MAX = 0.8;
 
 // ouie
-const double DIST_OUIE_MIN = 0.;
-const double DIST_OUIE_MAX = 0.;
+const double DIST_OUIE_MIN = 200.;
+const double DIST_OUIE_MAX = 201.;
 const double CAPA_OUIE_MIN = 0.1;
 const double CAPA_OUIE_MAX = 0.8;
+
+// seuil pour la bestiole peureuse
+const int S_PEUREUSE = 2;
 
 
 
